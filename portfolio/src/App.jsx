36589331969c4bf712f20d0289e97a749281f420
Sahil Randive <randivesahil7@gmail.com>
2024-05-13 20:22:31 +0530

    changes in ui

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -39,7 +39,7 @@ function App() {
                             <h1 className="font-bold text-2xl">Sahil Portfolio</h1>
                         </div>
                         <div>
-                            <ul className="flex gap-4">
+                            <ul className="flex gap-7">
                                 <li>
                                     <a href="#projects" className="text-gray-400 hover:text-white cursor-pointer">
                                         Projects
@@ -51,8 +51,8 @@ function App() {
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="#aboutme" className="text-gray-400 hover:text-white cursor-pointer">
-                                        About me
+                                    <a href="#contact me" className="text-gray-400 hover:text-white cursor-pointer">
+                                        Contact me
                                     </a>
                                 </li>
                             </ul>
@@ -93,10 +93,10 @@ function App() {
                             <div className="border border-gray-500 rounded-md p-5 flex-1">
                                 <img src={Weather} className="w-full h-auto" />
                                 <h3 className="text-2xl font-semibold mt-8">
-                                lorem ipsum dolor sit amet, consectetur adip
+                                WEATHER APP
                                 </h3>
                                 <p className="text-gray-400 text-sm mt-2">
-                                lorem ipsum dolor sit amet, consectetur adip
+                                Experience real-time weather updates at your fingertips, all presented in a visually stunning and easy-to-navigate interface.
                                 </p>
                                 <div className="flex mt-12 gap-2">
                                     <button onClick={() => window.open("https://sahil-randive-weather-app.netlify.app/", "_blank")} className="flex-1 text-sm py-3 bg-blue-400 border rounded-full hover:border-blue-500 ">
@@ -110,13 +110,13 @@ function App() {
                             <div className="border border-gray-500 rounded-md p-5 flex-1">
                                 <img src={Todo} className="w-full h-auto" />
                                 <h3 className="text-2xl font-semibold mt-8">
-                                lorem ipsum dolor sit amet, consectetur adip
+                                TODO APP
                                 </h3>
                                 <p className="text-gray-400 text-sm mt-2">
-                                lorem ipsum dolor sit amet, consectetur adip
+                                Boost your productivity with my sleek and intuitive todo list app powered by React.js. Organize your tasks effortlessly and stay on top of your priorities with a seamless user experience designed for maximum efficiency.
                                 </p>
                                 <div className="flex gap-2 mt-12">
-                                    <button onClick={() => window.open("https://sahil-todo.netlify.app/", "_blank")} className="flex-1 text-sm py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700">
+                                    <button onClick={() => window.open("https://sahil-todo.netlify.app/", "_blank")} className="flex-1 text-sm py-3 bg-blue-400 border rounded-full hover:border-blue-500 ">
                                         Live preview
                                     </button>
                                     <button className="flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500">
@@ -129,13 +129,13 @@ function App() {
                             <div className="border border-gray-500 rounded-md p-5 flex-1">
                                 <img src={Money} className="w-full h-auto" />
                                 <h3 className="text-2xl font-semibold mt-8">
-                                lorem ipsum dolor sit amet, consectetur adip
+                                CURRENCY CONVERTER
                                 </h3>
                                 <p className="text-gray-400 text-sm mt-2">
-                                lorem ipsum dolor sit amet, consectetur adip
+                                Explore global currencies effortlessly with my React.js-powered currency converter. Seamlessly convert between different currencies with just a few clicks, all within a modern and user-friendly interface.
                                 </p>
                                 <div className="flex gap-4 mt-12">
-                                    <button onClick={() => window.open("https://sahil-currency-converter.netlify.app/", "_blank")} className="flex-1 text-sm py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700">
+                                    <button onClick={() => window.open("https://sahil-currency-converter.netlify.app/", "_blank")} className="flex-1 text-sm py-3 bg-blue-400 border rounded-full hover:border-blue-500 ">
                                         Live preview
                                     </button>
                                     <button className="flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500 ">
@@ -146,13 +146,13 @@ function App() {
                             <div className="border border-gray-500 rounded-md p-5 flex-1">
                                 <img src={RandomQ} className="w-full h-auto" />
                                 <h3 className="text-2xl font-semibold mt-8">
-                                lorem ipsum dolor sit amet, consectetur adip
+                                QUOTE GENERATOR
                                 </h3>
                                 <p className="text-gray-400 text-sm mt-2">
-                                lorem ipsum dolor sit amet, consectetur adip
+                                Elevate your day with inspiring words from my dynamic quote generator, crafted with React.js. Immerse yourself in a collection of thought-provoking quotes that uplift, motivate, and spark creativity.
                                 </p>
                                 <div className="flex gap-2 mt-12">
-                                    <button onClick={() => window.open("https://sahil-quote-generator.netlify.app/", "_blank")} className="flex-1 text-sm py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700">
+                                    <button onClick={() => window.open("https://sahil-quote-generator.netlify.app/", "_blank")} className="flex-1 text-sm py-3 bg-blue-400 border rounded-full hover:border-blue-500 ">
                                         Live preview
                                     </button>
                                     <button className="flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500">
@@ -166,13 +166,13 @@ function App() {
                             <div className="border border-gray-500 rounded-md p-5 flex-1">
                                 <img src={Game} className="w-full h-auto" />
                                 <h3 className="text-2xl font-semibold mt-8">
-                                lorem ipsum dolor sit amet, consectetur adip
+                                TIC TAC TOE 
                                 </h3>
                                 <p className="text-gray-400 text-sm mt-2">
-                                lorem ipsum dolor sit amet, consectetur adip
+                                Dive into a classic game of strategy and fun with my Tic Tac Toe game, meticulously crafted using HTML, CSS, and JavaScript. Challenge your friends or test your skills in this timeless battle of X's and O's.
                                 </p>
                                 <div className="flex gap-4 mt-12">
-                                    <button onClick={() => window.open("https://sahil-tic-tac-toe-game.netlify.app/", "_blank")} className="flex-1 text-sm py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700">
+                                    <button onClick={() => window.open("https://sahil-tic-tac-toe-game.netlify.app/", "_blank")} className="flex-1 text-sm py-3 bg-blue-400 border rounded-full hover:border-blue-500 ">
                                         Live preview
                                     </button>
                                     <button className="flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500">
@@ -183,13 +183,13 @@ function App() {
                             <div className="border border-gray-500 rounded-md p-5 flex-1">
                                 <img src={News} className="w-full h-auto" />
                                 <h3 className="text-2xl font-semibold mt-8">
-                                lorem ipsum dolor sit amet, consectetur adip
+                                NEWS APP
                                 </h3>
                                 <p className="text-gray-400 text-sm mt-2">
-                                lorem ipsum dolor sit amet, consectetur adip
+                                Stay informed and up-to-date with my cutting-edge real-time news app. Harnessing the latest technologies, our app delivers breaking news and updates as they happen, straight to your fingertips.
                                 </p>
                                 <div className="flex gap-2 mt-12">
-                                    <button className="flex-1 text-sm py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700">
+                                    <button className="flex-1 text-sm py-3 bg-blue-400 border rounded-full hover:border-blue-500 ">
                                         Live preview
                                     </button>
                                     <button className="flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500">
@@ -260,7 +260,7 @@ function App() {
                     </div>
                 </section>
                 {/* Additional skills section */}
-                <section>
+                <section id="contact me">
                     <div className="container-lg">
                         <div className="flex flex-col md:flex-row w-full p-8 rounded-lg section-card">
                             <div className="flex flex-1 flex-col sm:gap-y-6 gap-y-4">
@@ -268,7 +268,10 @@ function App() {
                                     <h1>Contact Me</h1>
                                 </div>
                                 <div className="max-w-lg w-3/ text-label body-text">
-                                    <p>Let's chat about your ambitions, and we'll craft a killer portfolio that will blow minds and open doors you never knew existed.</p>
+                                    <p className="mb-6">Feel free to reach out using the contact form below. Simply fill in your name, email address, and message, and I'll get back to you as soon as possible.</p>
+                                    <p className="underline">Below is my Mail-id:</p>
+                                    <p>randivesahil7@gmail.com</p>
+
                                 </div>
                             </div>
                             <div className="grid grid-cols-1 flex-1">
@@ -294,9 +297,9 @@ function App() {
                                                 </div>
                                             </div>
                                             <div className="mb-5">
-                                                <label className="mt-2.5 block text-sm font-semibold leading-6 body-text">Last Name</label>
+                                                <label className="mt-2.5 block text-sm font-semibold leading-6 body-text">Message</label>
                                                 <div className="mt-2.5">
-                                                    <textarea className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" rows="7" placeholder="Enter your message">    
+                                                    <textarea className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" rows="5" placeholder="Enter your message">    
                                                     </textarea>
                                                 </div>
                                             </div>
